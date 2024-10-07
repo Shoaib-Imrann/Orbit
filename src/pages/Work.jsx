2326f872820a8e8f11761664e422bc0309330d65
IMRAN <100607395+Shoaib-Imrann@users.noreply.github.com>
2024-10-07 11:33:17 +0530

    Updated Work.jsx

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -8,7 +8,7 @@ import Marquee from "../components/Marquee";
 function Work() {
   return (
     <div className="w-full h-screen bg-[#f1f1f1]">
-      <div className="flex flex-col w-full h-[61vh] sm:h-[68vh] bg-[#54b0a4] pt-32 sm:pt-40">
+      <div className="flex flex-col w-full h-[58vh] sm:h-[68vh] bg-[#54b0a4] pt-32 sm:pt-40">
         <div>
           <h1 className='worktext uppercase text-[120px] sm:text-[152px] px-5 flex justify-center sm:justify-start items-center w-full sm:px-[70px] font-["Founders_Grotesk_X-Condensed"] font-semibold'>
             Work
@@ -18,16 +18,15 @@ function Work() {
           </div>
         </div>
         <div className="flex flex-col  h-full justify-end items-center z-50">
-          <div className="w-[80%] h-[48px] bg-[#343434] rounded-t-xl"></div>
-          <div className="w-[95%] h-[48px] bg-[#282828] rounded-t-xl"></div>
+          <div className="w-[80%] h-[33px] sm:h-[48px] bg-[#343434] rounded-t-xl"></div>
+          <div className="w-[95%] h-[38px] sm:h-[48px] bg-[#282828] rounded-t-xl"></div>
         </div>
       </div>
       <Featured
         text={
-          "Purpose driven, strategy-led presentations, that people care about."
+          "PROJECTS"
         }
-        textsize={"text-[40px]"}
-        visibility={"hidden"}
+        textsize={"text-[50px]"}
       />
       <Marquee text={"PERFECTION"} />
       <div>
@@ -41,3 +40,5 @@ function Work() {
 }
 
 export default Work;
+
+
